f06f564519a18f09fa402276b60a29aed004af85
Giovana Birck <gmattosbirck@my.bcit.ca>
2024-01-05 19:01:49 -0800

    new files and folders

---
+++ README.md
@@ -0,0 +1,12 @@
+## Leaflet Map
+
+## Documentation
+[React Leaflet](https://react-leaflet.js.org/)
+
+## Installations
+```npm install react react-dom leaflet```
+```npm install react-leaflet```
+
+```npm install -D @types/leaflet```
+
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic"
+
+const DynamicMap = dynamic(() => import('../../components/Map'), {ssr: false})
+
+// REASON using dynamic is because map will render in client side..
+
+export default function Home() {
+  return (
+    <DynamicMap/>
+  )
+}
